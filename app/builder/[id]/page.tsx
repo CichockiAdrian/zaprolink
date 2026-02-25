c50f0c262c972a7a1e89e91953e91b9cb8f4efb1
CichockiAdrian <adrian.cichocki@uczen.zsk.poznan.pl>
2026-02-26 00:32:34 +0100

    Fix: replace BlogPost with server component

--- app/builder/[id]/page.tsx
+++ app/builder/[id]/page.tsx
@@ -1,4 +1,4 @@
-import Builder from '../../../pages/Builder';
+import Builder from '../../pages/Builder';
 
 export default function BuilderIdPage({ params }: { params: { id: string } }) {
   return <Builder />;
