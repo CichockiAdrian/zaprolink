713f575bb8c2d6451a3e50d93589bf1fafc662a9
CichockiAdrian <144235155+CichockiAdrian@users.noreply.github.com>
2026-07-02 09:24:07 +0200

    Fix Vercel builder route compile error

--- app/builder/[id]/page.tsx
+++ app/builder/[id]/page.tsx
@@ -1,5 +1,5 @@
 import Builder from '../../pages/Builder';
 
-export default function BuilderIdPage({ params }: { params: { id: string } }) {
+export default function BuilderIdPage() {
   return <Builder />;
 }
